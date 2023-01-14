e8374ce71f0762214fb729b38fc6eb2ed951f252
aladinsane <alekudryavtsev@gmail.com>
2023-01-14 17:01:06 +0300

    deploy

--- backend/api/download_shopping_cart.py
+++ backend/api/download_shopping_cart.py
@@ -1,46 +1,54 @@
-from django.http import HttpResponse
-from fpdf import FPDF
-from recipe.models import Ingredients, RecipeIngredients
+import io
+from django.http import FileResponse
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 
-ingredients_for_shopping = {}
+from recipe.models import Ingredients, RecipeIngredients, Recipes
 
 
 def download_shopping_cart(shopping_ids):
+    ingredients_for_shopping = {}
+
     for shopping_id in shopping_ids:
         ingredients_obj = RecipeIngredients.objects.filter(
             recipe_id=shopping_id
         )
+        print(ingredients_obj)
         for ingredient_obj in ingredients_obj:
-            cart_obj_id = ingredient_obj.ingredients_id
-            amount = ingredient_obj.amount
+            cart_obj_id = ingredient_obj.related_ingredient_id
+            quantity = ingredient_obj.quantity
             measurement_unit = ingredient_obj.measurement_unit
 
             if cart_obj_id in ingredients_for_shopping.keys():
                 # Add amount for existing key
-                ingredients_for_shopping[cart_obj_id][0] += amount
+                ingredients_for_shopping[cart_obj_id][0] += quantity
 
             else:
                 # No key in data, add
-                ingredients_for_shopping[cart_obj_id] = [amount]
+                ingredients_for_shopping[cart_obj_id] = [quantity]
                 ingredients_for_shopping[cart_obj_id].append(measurement_unit)
 
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font("Arial", size=14)
-    pdf.set_title('Shopping list')
-    pdf.text(90, 20, 'Shopping list')
-    start_position = 40
+    pdfmetrics.registerFont(TTFont('Verdana', 'Verdana.ttf'))
+    buffer = io.BytesIO()
+
+    p = canvas.Canvas(buffer)
+    p.setFont("Verdana", 10)
+
+    p.drawString(50, 800, "Ingredients list")
+    y = 740
     for key in ingredients_for_shopping.keys():
         ingredient_name = Ingredients.objects.get(id=key)
-        pdf.text(10, start_position,
+        print(ingredients_for_shopping)
+        p.drawString(50, y,
                  f'{str(ingredient_name)} ('
                  f'{str(ingredients_for_shopping[key][1])}) '
                  f'— {str(ingredients_for_shopping[key][0])}')
 
-        start_position += 20
-    pdf_filename = pdf.output("shopping_cart.pdf")
-    response = HttpResponse(pdf, content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="{}"'.format(
-        pdf_filename
-    )
-    return response
+        y = y + 20
+
+    p.showPage()
+    p.save()
+
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
