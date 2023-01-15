f6776c76a458446e89c3fa80d3e18ee77caf94b4
aladinsane <alekudryavtsev@gmail.com>
2023-01-15 13:29:17 +0300

    deploy

--- backend/users/models.py
+++ backend/users/models.py
@@ -52,7 +52,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     class Meta:
